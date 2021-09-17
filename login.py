dd1162d446903f74e20ee6b67ecf783eb802463f
Richard-Zheng <yuqiu36@live.com>
2021-09-17 22:03:33 +0800

    it works!

--- login.py
+++ login.py
@@ -1,11 +1,18 @@
 import argparse
+import asyncio
+from pathlib import Path
+
+import aiohttp
 import requests
 import config
 import json
 from html.parser import HTMLParser
+from ics import Calendar, Event
 
 import crypto
 
+data_dir = Path('data')
+data_dir.mkdir(parents=True, exist_ok=True)
 
 class AuthHTMLParser(HTMLParser):
     def __init__(self, username, password):
@@ -25,7 +32,8 @@ class AuthHTMLParser(HTMLParser):
 
 
 class UserSession:
-    def __init__(self, username: str, password: str):
+    def __init__(self, session: aiohttp.ClientSession, username: str, password: str):
+        self.session = session
         self.username = username
         self.password = password
         self.authHTML = AuthHTMLParser(username, password)
@@ -34,6 +42,7 @@ class UserSession:
             headers=config.headers)
         self.cookies = response.cookies
         self.authHTML.feed(response.text)
+        self.login()
 
     def login(self):
         response = requests.post(
@@ -44,17 +53,55 @@ class UserSession:
         )
         self.cookies = response.history[2].cookies
 
-    def get_courses_date(self, school_year: int, term: int, week: int):
-        response = requests.post(
+    async def get_week_schedule(self, school_year: int, term: int, week: int):
+        async with self.session.post(
             config.GetStudentCoursesDateURL,
             params={
                 'xnxqdm': f'{str(school_year)}0{str(term)}', # 学年学期
                 'zc': str(week) # 周次
             },
             cookies=self.cookies
-        )
-        return json.loads(response.text)
+        ) as response:
+            return json.loads(await response.text())
 
+    async def get_term_schedule(self, school_year: int, term: int):
+        term_data_path = Path(data_dir, f'user_{self.username}_{school_year}0{term}_schedule.txt')
+        if term_data_path.exists():
+            with term_data_path.open(mode='r') as f:
+                return json.load(f)
+        task = []
+        for week in range(1, 20):
+            task.append(self.get_week_schedule(school_year, term, week))
+        all_schedule_in_a_term = await asyncio.gather(*task)
+        with term_data_path.open(mode='w') as f:
+            json.dump(all_schedule_in_a_term, f)
+        return all_schedule_in_a_term
+
+    async def get_term_ics(self, school_year: int, term: int):
+        term_ics_path = Path(data_dir, f'user_{self.username}_{school_year}0{term}_schedule.ics')
+        term_schedule = await self.get_term_schedule(school_year, term)
+        c = Calendar()
+        for week_schedule in term_schedule:
+            day_to_date = {}
+            for day in week_schedule[1]:
+                day_to_date[day['xqmc']] = day['rq']
+
+            for course in week_schedule[0]:
+                e = Event()
+                e.name = course['kcmc']
+                jcArr = course['jcdm2'].split(",")
+                e.begin = day_to_date[course['xq']] + ' ' + config.jc_to_time[int(jcArr[0])][0] + '+08:00'
+                e.end = day_to_date[course['xq']] + ' ' + config.jc_to_time[int(jcArr[-1])][1] + '+08:00'
+                e.location = course['jxcdmc']
+                c.events.add(e)
+        with term_ics_path.open(mode='w') as f:
+            f.write(str(c))
+
+
+async def main(args):
+    async with aiohttp.ClientSession() as session:
+        us = UserSession(session, args.username, args.password)
+        await us.get_term_ics(2021, 1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -62,7 +109,5 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--password")
     args = parser.parse_args()
 
-    us = UserSession(args.username, args.password)
-    us.login()
-    c = us.get_courses_date(2021, 1, 4)
-    print(c)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(args))
